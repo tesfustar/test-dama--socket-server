b80d33aa6ba05f482c7816dc793daeadb0ef6edb
Tesfaye Lemi <tesfu9503@gmail.com>
2023-01-01 09:35:59 -0800

    reset game message request added

--- server.js
+++ server.js
@@ -4,51 +4,50 @@ import { instrument } from "@socket.io/admin-ui";
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
-    origin: ["https://dama-game-socketio.vercel.app",
-    "http://localhost:3000",
-    "https://dama-blue.vercel.app","https://admin.socket.io"],
-    credentials: true
+    origin: [
+      "https://dama-game-socketio.vercel.app",
+      "http://localhost:3000","http://192.168.0.143:3000",
+      "https://dama-blue.vercel.app",
+      "https://admin.socket.io",
+    ],
+    credentials: true,
   },
 });
 
-
-
-
-
 io.on("connection", (socket) => {
-// console.log(socket.id)
+  // console.log(socket.id)
   //user connection
   console.log("a user connected.");
 
-   socket.on('join-room',async(room)=>{
-
-     const clients =await io.of('/').in(room).fetchSockets()
-      if(clients.length == 2 ){
-
-        // io.to(room).emit("started","you can play now")
-        io.to(socket.id).emit("roomTwo","room is filled")
-      }else{
-        socket.join(room)
-        io.to(room).emit("private-room","you are now in private room")
-      }
+  socket.on("join-room", async (room) => {
+    const clients = await io.of("/").in(room).fetchSockets();
+    if (clients.length == 2) {
+      // io.to(room).emit("started","you can play now")
+      io.to(socket.id).emit("roomTwo", "room is filled");
+    } else {
+      socket.join(room);
+      io.to(room).emit("private-room", "you are now in private room");
+    }
     //send and get message
-  
+
     socket.on("sendMessage", (data) => {
-      io.to(room).emit("getMessage", data)
+      io.to(room).emit("getMessage", data);
     });
     socket.on("sendGameMessage", (data) => {
-      io.to(room).emit("getGameMessage", data)
+      io.to(room).emit("getGameMessage", data);
     });
-   })
+    socket.on("sendResetGameMessage", (data) => {
+      io.to(room).broadcast.emit("getResetGameMessage", data);
+    });
+  });
 
   //when disconnect
   socket.on("disconnect", () => {
-    console.log("a user disconnected!")
+    console.log("a user disconnected!");
   });
 });
 instrument(io, {
-  auth: false
+  auth: false,
 });
 const PORT = process.env.PORT || 7744;
 httpServer.listen(PORT);
-
