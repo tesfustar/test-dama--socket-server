8acca8b1b962d37d0440eb758b0739537d539a62
TesfuStar <104075266+TesfuStar@users.noreply.github.com>
2023-03-09 09:57:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const io = new Server(httpServer, {
       "http://172.17.104.251:3000",
       "http://192.168.43.253:3000",
       "http://172.17.104.250:3000",
-      "http://172.17.104.246:3000",
+      "http://172.17.104.249:3000",
       "http://172.17.104.254:3000",
       "https://dama-blue.vercel.app",
       "https://admin.socket.io",
